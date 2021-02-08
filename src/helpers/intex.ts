82146fd6169db8d0ded2242ce623faa54298cbc0
Ivan Nazarenko <nazarenkoivan@protonmail.com>
2021-02-08 23:24:08 +0200

    Add calendar event class

--- src/helpers/intex.ts
+++ src/helpers/intex.ts
@@ -3,3 +3,23 @@ export const validationMessages = {
         email: 'Введіть валідний email',
     }
 };
+
+export default class CalendarEvent {
+    title: string;
+    allDay: boolean;
+    start: Date;
+    end: Date;
+    desc: string;
+    resourceId?: string;
+    tooltip?: string;
+
+    constructor(_title: string, _start: Date, _endDate: Date, _allDay?: boolean, _desc?: string, _resourceId?: string) {
+        this.title = _title;
+        this.allDay = _allDay || false;
+        this.start = _start;
+        this.end = _endDate;
+        this.desc = _desc || '';
+        this.resourceId = _resourceId;
+    }
+};
+
