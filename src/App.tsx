7b796af9ca04365f8872d2ecac38ee037863e989
Ivan Nazarenko <nazarenkoivan@protonmail.com>
2021-02-09 21:34:02 +0200

    Change router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Routes from './routes';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
